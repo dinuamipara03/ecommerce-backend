3c14546d8cfe5e4d114f1072db6418b4a66a7352
dinuamipara03 <143832639+dinuamipara03@users.noreply.github.com>
2025-05-05 19:01:59 +0530

    Update database

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,16 +1,13 @@
 import { registerAs } from "@nestjs/config";
 import { config as dotenvConfig } from 'dotenv';
+import { url } from "inspector";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 dotenvConfig({ path: '.env' });
 
 const config = {
-    type: 'mysql',
-    host: process.env.DATABASE_HOST || 'localhost', // Default to localhost if undefined
-    port: parseInt(process.env.DATABASE_PORT || '3306', 10), // Convert port to number
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || '',
-    database: process.env.DATABASE_NAME || 'ecommerce',
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
     entities: ["dist/entity/*{.ts,.js}"],
     migrations: ["dist/migrations/*{.ts,.js}"],
     autoLoadEntities: true,
